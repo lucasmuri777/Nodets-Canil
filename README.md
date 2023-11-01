7459f2ad465af0ca16a8f7610860ed376e7d2fdb
lucasmuri777 <lucasmuri777@gmail.com>
2023-11-01 19:34:52 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Nodets-Canil
+
+### INICIAR PROJETO:
+`npm init`
+
+### INICIAR TYPESCRIPT:
+`tsc --init`
+
+### Mudar no tsconfig:
+`"rootDir": "./src", "outDir": "./dist", "moduleResolution": "node"`
+
+### DEPENDENCIAS:
+`npm install express mustache-express dotenv (EXPRESS, mustache, dotenv)`
+
+### DEPENDENCIES DE DESENVOLVIMENTO:
+`npm install --save-dev @types/express @types/mustache-express @types/node`
+
+### Instalação global (Caso tenha clonado o repo):
+`npm install` and `npm install --save-dev`
+
+### Rodar o projeto:
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes/index';
+
+dotenv.config();//configurou o dotenv para ser usado na aplicação
+
+const server = express();
+
+server.set('view engine', 'mustache');//configurou o mustache para ser usado na aplicação
+server.set('views', path.join(__dirname, 'views'));//configurou o path para ser usado na aplicação
+server.engine('mustache', mustache());//configurou o mustache para ser usado
+
+server.use(express.static(path.join(__dirname, '../public')));//configurou uma pasta de arquivos estaticos para ser usado
+
+
+//Rotas
+server.use(mainRoutes);
+
+server.use((req, res)=>{
+    res.send('Página não encontrada')
+})//Se não for nenhuma das rotas
+
+
+//Inicia servidos
+server.listen(process.env.PORT);
